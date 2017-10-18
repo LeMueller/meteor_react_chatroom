3a065af6fc6610f1e946e616744720567db0ddb3
lezahng <lezhang@techfak.uni-bielefeld.de>
2017-10-18 13:10:24 +0200

    send message ok

--- imports/ui/SendArea.jsx
+++ imports/ui/SendArea.jsx
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import {DialogServer} from './dialogServer.js';
 
 export default class SendArea extends Component{
-  render(){
-    return(
-      <div>
-        <input type='text'/>
-        <button>send</button>
-      </div>
-    )
-  }
+
+	constructor(props){
+	    super(props);
+	}
+
+	handleSubmit(event) {
+	    event.preventDefault();
+	 
+	    // Find the text field via the React ref
+	    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+	 
+	    DialogServer.insert({
+	      text: text,
+	      sender:"self",
+	      createdAt: new Date(), // current time
+	    });
+	 
+	    // Clear form
+	    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+	}
+
+	render(){
+	    return(
+	      <div>
+	      	<form className="new_message" onSubmit={this.handleSubmit.bind(this)}>
+		        <input type='text' ref="textInput" placeholder="message to send"/>
+		        <button onClick={this.handleSubmit.bind(this)}>send</button>
+	        </form>
+	      </div>
+	    )
+	 }
 }
